5b115f465ecbcdce7fd87de423d3677982f7c442
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-13 16:45:45 +0530

    feat(config): added basic config parsing and combination

--- src/config.rs
+++ src/config.rs
@@ -1,34 +1,110 @@
-use crate::utils::get_git_dir_path;
+use crate::utils::{get_git_dir_path, merge_hashmaps};
 use home::home_dir;
+use ini::configparser::ini::Ini;
+use std::{collections::HashMap, env, path::Path};
 
+#[derive(Debug)]
 pub enum ConfigLocation {
     Local,
     Global,
     System,
-    WinUserRoot,
-    Unknown,
 }
 
-pub fn locate_config_file() -> ConfigLocation {
+pub fn get_merged_config() -> (
+    HashMap<String, HashMap<String, Option<String>>>,
+    HashMap<String, ConfigLocation>,
+) {
     let base_git_dir = get_git_dir_path();
     let local_config_path = base_git_dir.join("config");
+    let global_config_path = home_dir()
+        .unwrap()
+        .join(".config")
+        .join("git")
+        .join("config");
+    let global_config_path_2 = home_dir().unwrap().join(".gitconfig");
+
+    let system_config_path = if cfg!(windows) {
+        Path::new(env::var("ProgramFiles").unwrap().as_str())
+            .join("Git")
+            .join("etc")
+            .join("gitconfig")
+    } else {
+        Path::new("/etc").join("gitconfig")
+    };
+
+    let mut config: HashMap<String, HashMap<String, Option<String>>> = HashMap::new();
+    let mut locs: HashMap<String, ConfigLocation> = HashMap::new();
+
+    if system_config_path.exists() {
+        let mut system_config = Ini::new();
+        match system_config.load(system_config_path.to_str().unwrap()) {
+            Ok(sconfig) => {
+                for (k, v) in &sconfig {
+                    for (k_i, _) in v {
+                        locs.insert(k.to_string() + "." + k_i.as_str(), ConfigLocation::System);
+                    }
+                }
+                config = merge_hashmaps(config, sconfig);
+            }
+            _ => {}
+        }
+    }
+
+    if global_config_path.exists() {
+        let mut global_config = Ini::new();
+        match global_config.load(global_config_path.to_str().unwrap()) {
+            Ok(gconfig) => {
+                for (k, v) in &gconfig {
+                    for (k_i, _) in v {
+                        locs.insert(k.to_string() + "." + k_i.as_str(), ConfigLocation::Global);
+                    }
+                }
+                config = merge_hashmaps(config, gconfig);
+            }
+            _ => {}
+        }
+    } else if global_config_path_2.exists() {
+        let mut global_config = Ini::new();
+        match global_config.load(global_config_path_2.to_str().unwrap()) {
+            Ok(gconfig) => {
+                for (k, v) in &gconfig {
+                    for (k_i, _) in v {
+                        locs.insert(k.to_string() + "." + k_i.as_str(), ConfigLocation::Global);
+                    }
+                }
+                config = merge_hashmaps(config, gconfig);
+            }
+            _ => {}
+        }
+    }
 
     if local_config_path.exists() {
-        return ConfigLocation::Local;
+        let mut local_config = Ini::new();
+        match local_config.load(local_config_path.to_str().unwrap()) {
+            Ok(lconfig) => {
+                for (k, v) in &lconfig {
+                    for (k_i, _) in v {
+                        locs.insert(k.to_string() + "." + k_i.as_str(), ConfigLocation::Local);
+                    }
+                }
+                config = merge_hashmaps(config, lconfig);
+            }
+            _ => {}
+        }
     }
 
-    match home_dir() {
-        Some(path) => {
-            if path.join(".gitconfig").exists()
-                || path.join(".config").join("git").join("config").exists()
-            {
-                return ConfigLocation::Global;
-            } else {
-                return ConfigLocation::System;
-            }
-        }
-        _ => {
-            return ConfigLocation::Unknown;
-        }
-    }
+    // for debugging purposes
+    // for (k, v) in &config {
+    //     for (k_i, v_i) in v {
+    //         println!(
+    //             "{:?} {}.{}={}",
+    //             locs[&k.clone().add(".").add(k_i)],
+    //             k,
+    //             k_i,
+    //             v_i.clone().unwrap()
+    //         );
+    //     }
+    // }
+
+    return (config, locs);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     env,
     path::{Path, PathBuf},
     process::exit,
@@ -17,4 +18,49 @@ pub fn get_git_dir_path() -> PathBuf {
             exit(1);
         }
     }
+}
+
+pub fn merge_hashmaps(
+    hm_1: HashMap<String, HashMap<String, Option<String>>>,
+    hm_2: HashMap<String, HashMap<String, Option<String>>>,
+) -> HashMap<String, HashMap<String, Option<String>>> {
+    let mut final_hash: HashMap<String, HashMap<String, Option<String>>> = HashMap::new();
+
+    for (key, val) in hm_1 {
+        if final_hash.contains_key(&key) {
+            let mut inner_final_hash: HashMap<String, Option<String>> = HashMap::new();
+
+            for (key_i, val_i) in val {
+                inner_final_hash.insert(key_i, val_i);
+            }
+            
+            for (key_i, val_i) in &final_hash[&key] {
+                inner_final_hash.insert(key_i.to_string(), val_i.clone());
+            }
+
+            final_hash.insert(key, inner_final_hash);
+        } else {
+            final_hash.insert(key, val);
+        }
+    }
+
+    for (key, val) in hm_2 {
+        if final_hash.contains_key(&key) {
+            let mut inner_final_hash: HashMap<String, Option<String>> = HashMap::new();
+
+            for (key_i, val_i) in val {
+                inner_final_hash.insert(key_i, val_i);
+            }
+
+            for (key_i, val_i) in &final_hash[&key] {
+                inner_final_hash.insert(key_i.to_string(), val_i.clone());
+            }
+
+            final_hash.insert(key, inner_final_hash);
+        } else {
+            final_hash.insert(key, val);
+        }
+    }
+
+    return final_hash;
 }
