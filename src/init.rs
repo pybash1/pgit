e598b1f87ffb76b295a4d6d6ba50614cb8af229c
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-14 09:29:17 +0530

    feat(init): added support for basic flags in init command

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,6 @@
 use crate::utils::get_git_dir_path;
-use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 fn already_init() -> bool {
     let mut inited = false;
@@ -18,7 +18,20 @@ pub fn init_repo(
     separate_git_dir: Option<PathBuf>,
     branch_name: Option<&str>,
 ) {
-    let base_git_dir = get_git_dir_path();
+    // println!(
+    //     "{:#?}",
+    //     if separate_git_dir.is_some() {
+    //         separate_git_dir.unwrap()
+    //     } else {
+    //         Path::new("").to_path_buf()
+    //     }
+    // );
+
+    let base_git_dir = if !bare.unwrap() {
+        get_git_dir_path()
+    } else {
+        env::current_dir().unwrap()
+    };
     let objects_git_dir = base_git_dir.join("objects");
     let objects_info_git_dir = objects_git_dir.join("info");
     let objects_pack_git_dir = objects_git_dir.join("pack");
@@ -28,7 +41,6 @@ pub fn init_repo(
     let hooks_git_dir = base_git_dir.join("hooks");
     let info_git_dir = base_git_dir.join("info");
     let head_git_file = base_git_dir.join("HEAD");
-    let fetch_head_git_file = base_git_dir.join("FETCH_HEAD");
     let config_git_file = base_git_dir.join("config");
     let description_git_file = base_git_dir.join("description");
 
@@ -47,8 +59,12 @@ pub fn init_repo(
         fs::create_dir_all(refs_tags_git_dir).unwrap();
         fs::create_dir_all(hooks_git_dir).unwrap();
         fs::create_dir_all(info_git_dir).unwrap();
-        fs::write(head_git_file, "ref: refs/heads/master\n").unwrap();
-        fs::write(fetch_head_git_file, "").unwrap();
+
+        fs::write(
+            head_git_file,
+            String::from("ref: refs/head/") + branch_name.unwrap_or("master") + "\n",
+        )
+        .unwrap();
         fs::write(config_git_file, "").unwrap();
         fs::write(
             description_git_file,
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,61 @@ mod config;
 mod init;
 mod utils;
 
+use std::path::Path;
+
 use crate::init::init_repo;
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
+    let init = Command::new("init")
+        .about("Initialize or reinitialize a local git repository")
+        .args([
+            Arg::new("quiet")
+                .long("quiet")
+                .short('q')
+                .action(ArgAction::SetTrue)
+                .help("Don't output anything"),
+            Arg::new("bare")
+                .long("bare")
+                .action(ArgAction::SetTrue)
+                .help("Create files in current directory"),
+            Arg::new("branch")
+                .long("initial-branch")
+                .short('b')
+                .action(ArgAction::Set)
+                .help("Override initial branch name")
+                .value_name("name"),
+            Arg::new("gitdir")
+                .long("separate-git-dir")
+                .action(ArgAction::Set)
+                .help("Create repository in separate directory from working tree"),
+        ]);
+
     let cli = Command::new("pgit")
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
         .about("An alternative Git CLI which is actually understandable.")
-        .subcommand(Command::new("init").about("Initialize or reinitialize a local git repository"))
+        .subcommand(init)
         .arg_required_else_help(true);
 
     match cli.try_get_matches() {
         Ok(matches) => match matches.subcommand_name() {
             Some("init") => {
-                init_repo(None, None, None, None);
+                let args = matches.subcommand().unwrap().1.to_owned();
+                init_repo(
+                    Some(args.get_one::<bool>("quiet").unwrap().to_owned()),
+                    Some(args.get_one::<bool>("bare").unwrap().to_owned()),
+                    if args.get_one::<String>("gitdir").is_some() {
+                        Some(Path::new(args.get_one::<String>("gitdir").unwrap()).to_path_buf())
+                    } else {
+                        None
+                    },
+                    if args.get_one::<String>("branch").is_some() {
+                        Some(args.get_one::<String>("branch").unwrap())
+                    } else {
+                        None
+                    },
+                );
             }
             _ => unreachable!("All exception cases are handled by clap"),
         },
